9ebae206ddbf808d8c0f366c04f4d3e5124aa6f6
taachii <taachii21@outlook.com>
2024-05-14 13:46:52 +0200

    Update homepage view

--- app/views/home_view.tpl
+++ app/views/home_view.tpl
@@ -1,11 +1,163 @@
 {extends file="main.tpl"}
 
 {block name=content}
-  <div class="container-fluid">
-      <div class="row">
-        <div class="col-lg-12 d-flex justify-content-center">
-          <img src="{$conf->app_url}/assets/images/banner.jpg" alt="">
-        </div>
+ <div class="container-fluid">
+  <div class="row page-titles mx-0">
+    <div class="col-sm-6 p-md-0">
+      <div class="welcome-text">
+        <h4>Witaj, {$user['username']}!</h4>
+        <span class="ml-1">Dodaj, usuń lub edytuj swoje notatki treningowe!</span>
       </div>
+    </div>
+    <div class="col-sm-6 justify-content-sm-end mt-2 mt-sm-0 d-flex">
+      <a href="javascript:void()" class="btn btn-info">Nowa notatka</a>
+    </div>
   </div>
+  <div class="row">
+    <div class="col-xl-4 col-xxl-6 col-lg-6 col-sm-6 ">
+      <div class="card">
+          <div class="card-header">
+            <h5 class="card-title">Card title</h5>
+          </div>
+          <div class="card-body">
+            <p class="card-text">This is a wider card with supporting text and below as a natural lead-in to the additional content. This content is a little <br> bit longer. Some quick example text to build the bulk</p>
+          </div>
+          <div class="card-footer d-sm-flex justify-content-between">
+            <div class="card-footer-link mb-4 mb-sm-0">
+              <p class="card-text text-dark d-inline">Last updated 3 mins ago</p>
+            </div>
+            <a href="javascript:void()" class="btn btn-primary">Go somewhere</a>
+          </div>
+      </div>
+    </div>
+    <div class="col-xl-4 col-xxl-6 col-lg-6 col-sm-6 ">
+      <div class="card">
+          <div class="card-header">
+            <h5 class="card-title">Card title</h5>
+          </div>
+          <div class="card-body">
+            <p class="card-text">This is a wider card with supporting text and below as a natural lead-in to the additional content. This content is a little <br> bit longer. Some quick example text to build the bulk</p>
+          </div>
+          <div class="card-footer d-sm-flex justify-content-between">
+            <div class="card-footer-link mb-4 mb-sm-0">
+              <p class="card-text text-dark d-inline">Last updated 3 mins ago</p>
+            </div>
+            <a href="javascript:void()" class="btn btn-primary">Go somewhere</a>
+          </div>
+      </div>
+    </div>
+    <div class="col-xl-4 col-xxl-6 col-lg-6 col-sm-6 ">
+      <div class="card">
+          <div class="card-header">
+            <h5 class="card-title">Card title</h5>
+          </div>
+          <div class="card-body">
+            <p class="card-text">This is a wider card with supporting text and below as a natural lead-in to the additional content. This content is a little <br> bit longer. Some quick example text to build the bulk</p>
+          </div>
+          <div class="card-footer d-sm-flex justify-content-between">
+            <div class="card-footer-link mb-4 mb-sm-0">
+              <p class="card-text text-dark d-inline">Last updated 3 mins ago</p>
+            </div>
+            <a href="javascript:void()" class="btn btn-primary">Go somewhere</a>
+          </div>
+      </div>
+    </div>
+    <div class="col-xl-4 col-xxl-6 col-lg-6 col-sm-6 ">
+      <div class="card">
+          <div class="card-header">
+            <h5 class="card-title">Card title</h5>
+          </div>
+          <div class="card-body">
+            <p class="card-text">This is a wider card with supporting text and below as a natural lead-in to the additional content. This content is a little <br> bit longer. Some quick example text to build the bulk</p>
+          </div>
+          <div class="card-footer d-sm-flex justify-content-between">
+            <div class="card-footer-link mb-4 mb-sm-0">
+              <p class="card-text text-dark d-inline">Last updated 3 mins ago</p>
+            </div>
+            <a href="javascript:void()" class="btn btn-primary">Go somewhere</a>
+          </div>
+      </div>
+    </div>
+    <div class="col-xl-4 col-xxl-6 col-lg-6 col-sm-6 ">
+      <div class="card">
+          <div class="card-header">
+            <h5 class="card-title">Card title</h5>
+          </div>
+          <div class="card-body">
+            <p class="card-text">This is a wider card with supporting text and below as a natural lead-in to the additional content. This content is a little <br> bit longer. Some quick example text to build the bulk</p>
+          </div>
+          <div class="card-footer d-sm-flex justify-content-between">
+            <div class="card-footer-link mb-4 mb-sm-0">
+              <p class="card-text text-dark d-inline">Last updated 3 mins ago</p>
+            </div>
+            <a href="javascript:void()" class="btn btn-primary">Go somewhere</a>
+          </div>
+      </div>
+    </div>
+    <div class="col-xl-4 col-xxl-6 col-lg-6 col-sm-6 ">
+      <div class="card">
+          <div class="card-header">
+            <h5 class="card-title">Card title</h5>
+          </div>
+          <div class="card-body">
+            <p class="card-text">This is a wider card with supporting text and below as a natural lead-in to the additional content. This content is a little <br> bit longer. Some quick example text to build the bulk</p>
+          </div>
+          <div class="card-footer d-sm-flex justify-content-between">
+            <div class="card-footer-link mb-4 mb-sm-0">
+              <p class="card-text text-dark d-inline">Last updated 3 mins ago</p>
+            </div>
+            <a href="javascript:void()" class="btn btn-primary">Go somewhere</a>
+          </div>
+      </div>
+    </div>
+    <div class="col-xl-4 col-xxl-6 col-lg-6 col-sm-6 ">
+      <div class="card">
+          <div class="card-header">
+            <h5 class="card-title">Card title</h5>
+          </div>
+          <div class="card-body">
+            <p class="card-text">This is a wider card with supporting text and below as a natural lead-in to the additional content. This content is a little <br> bit longer. Some quick example text to build the bulk</p>
+          </div>
+          <div class="card-footer d-sm-flex justify-content-between">
+            <div class="card-footer-link mb-4 mb-sm-0">
+              <p class="card-text text-dark d-inline">Last updated 3 mins ago</p>
+            </div>
+            <a href="javascript:void()" class="btn btn-primary">Go somewhere</a>
+          </div>
+      </div>
+    </div>
+    <div class="col-xl-4 col-xxl-6 col-lg-6 col-sm-6 ">
+      <div class="card">
+          <div class="card-header">
+            <h5 class="card-title">Card title</h5>
+          </div>
+          <div class="card-body">
+            <p class="card-text">This is a wider card with supporting text and below as a natural lead-in to the additional content. This content is a little <br> bit longer. Some quick example text to build the bulk</p>
+          </div>
+          <div class="card-footer d-sm-flex justify-content-between">
+            <div class="card-footer-link mb-4 mb-sm-0">
+              <p class="card-text text-dark d-inline">Last updated 3 mins ago</p>
+            </div>
+            <a href="javascript:void()" class="btn btn-primary">Go somewhere</a>
+          </div>
+      </div>
+    </div>
+    <div class="col-xl-4 col-xxl-6 col-lg-6 col-sm-6 ">
+      <div class="card">
+          <div class="card-header">
+            <h5 class="card-title">Card title</h5>
+          </div>
+          <div class="card-body">
+            <p class="card-text">This is a wider card with supporting text and below as a natural lead-in to the additional content. This content is a little <br> bit longer. Some quick example text to build the bulk</p>
+          </div>
+          <div class="card-footer d-sm-flex justify-content-between">
+            <div class="card-footer-link mb-4 mb-sm-0">
+              <p class="card-text text-dark d-inline">Last updated 3 mins ago</p>
+            </div>
+            <a href="javascript:void()" class="btn btn-primary">Go somewhere</a>
+          </div>
+      </div>
+    </div>
+  </div>
+ </div>
 {/block}
--- public/templates_c/7850475455a4fcbc7f23f6a7daedac7781142137_0.file.home_view.tpl.php
+++ public/templates_c/7850475455a4fcbc7f23f6a7daedac7781142137_0.file.home_view.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.3.4, created on 2024-05-14 12:09:59
+/* Smarty version 4.3.4, created on 2024-05-14 13:44:10
   from 'D:\xampp\htdocs\fit-tracker\app\views\home_view.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.3.4',
-  'unifunc' => 'content_66433877b7a817_37204056',
+  'unifunc' => 'content_66434e8a66dca6_00221565',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '7850475455a4fcbc7f23f6a7daedac7781142137' => 
     array (
       0 => 'D:\\xampp\\htdocs\\fit-tracker\\app\\views\\home_view.tpl',
-      1 => 1715681398,
+      1 => 1715687048,
       2 => 'file',
     ),
   ),
@@ -20,36 +20,188 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_66433877b7a817_37204056 (Smarty_Internal_Template $_smarty_tpl) {
+function content_66434e8a66dca6_00221565 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_loadInheritance();
 $_smarty_tpl->inheritance->init($_smarty_tpl, true);
 ?>
 
 
 <?php 
-$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_96179550066433877b77396_00802346', 'content');
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_37982731966434e8a66a123_64402898', 'content');
 $_smarty_tpl->inheritance->endChild($_smarty_tpl, "main.tpl");
 }
 /* {block 'content'} */
-class Block_96179550066433877b77396_00802346 extends Smarty_Internal_Block
+class Block_37982731966434e8a66a123_64402898 extends Smarty_Internal_Block
 {
 public $subBlocks = array (
   'content' => 
   array (
-    0 => 'Block_96179550066433877b77396_00802346',
+    0 => 'Block_37982731966434e8a66a123_64402898',
   ),
 );
 public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
 ?>
 
-  <div class="container-fluid">
-      <div class="row">
-        <div class="col-lg-12 d-flex justify-content-center">
-          <img src="<?php echo $_smarty_tpl->tpl_vars['conf']->value->app_url;?>
-/assets/images/banner.jpg" alt="">
-        </div>
+ <div class="container-fluid">
+  <div class="row page-titles mx-0">
+    <div class="col-sm-6 p-md-0">
+      <div class="welcome-text">
+        <h4>Witaj, <?php echo $_smarty_tpl->tpl_vars['user']->value['username'];?>
+!</h4>
+        <span class="ml-1">Dodaj, usuń lub edytuj swoje notatki treningowe!</span>
       </div>
+    </div>
+    <div class="col-sm-6 justify-content-sm-end mt-2 mt-sm-0 d-flex">
+      <a href="javascript:void()" class="btn btn-info">Nowa notatka</a>
+    </div>
   </div>
+  <div class="row">
+    <div class="col-xl-4 col-xxl-6 col-lg-6 col-sm-6 ">
+      <div class="card">
+          <div class="card-header">
+            <h5 class="card-title">Card title</h5>
+          </div>
+          <div class="card-body">
+            <p class="card-text">This is a wider card with supporting text and below as a natural lead-in to the additional content. This content is a little <br> bit longer. Some quick example text to build the bulk</p>
+          </div>
+          <div class="card-footer d-sm-flex justify-content-between">
+            <div class="card-footer-link mb-4 mb-sm-0">
+              <p class="card-text text-dark d-inline">Last updated 3 mins ago</p>
+            </div>
+            <a href="javascript:void()" class="btn btn-primary">Go somewhere</a>
+          </div>
+      </div>
+    </div>
+    <div class="col-xl-4 col-xxl-6 col-lg-6 col-sm-6 ">
+      <div class="card">
+          <div class="card-header">
+            <h5 class="card-title">Card title</h5>
+          </div>
+          <div class="card-body">
+            <p class="card-text">This is a wider card with supporting text and below as a natural lead-in to the additional content. This content is a little <br> bit longer. Some quick example text to build the bulk</p>
+          </div>
+          <div class="card-footer d-sm-flex justify-content-between">
+            <div class="card-footer-link mb-4 mb-sm-0">
+              <p class="card-text text-dark d-inline">Last updated 3 mins ago</p>
+            </div>
+            <a href="javascript:void()" class="btn btn-primary">Go somewhere</a>
+          </div>
+      </div>
+    </div>
+    <div class="col-xl-4 col-xxl-6 col-lg-6 col-sm-6 ">
+      <div class="card">
+          <div class="card-header">
+            <h5 class="card-title">Card title</h5>
+          </div>
+          <div class="card-body">
+            <p class="card-text">This is a wider card with supporting text and below as a natural lead-in to the additional content. This content is a little <br> bit longer. Some quick example text to build the bulk</p>
+          </div>
+          <div class="card-footer d-sm-flex justify-content-between">
+            <div class="card-footer-link mb-4 mb-sm-0">
+              <p class="card-text text-dark d-inline">Last updated 3 mins ago</p>
+            </div>
+            <a href="javascript:void()" class="btn btn-primary">Go somewhere</a>
+          </div>
+      </div>
+    </div>
+    <div class="col-xl-4 col-xxl-6 col-lg-6 col-sm-6 ">
+      <div class="card">
+          <div class="card-header">
+            <h5 class="card-title">Card title</h5>
+          </div>
+          <div class="card-body">
+            <p class="card-text">This is a wider card with supporting text and below as a natural lead-in to the additional content. This content is a little <br> bit longer. Some quick example text to build the bulk</p>
+          </div>
+          <div class="card-footer d-sm-flex justify-content-between">
+            <div class="card-footer-link mb-4 mb-sm-0">
+              <p class="card-text text-dark d-inline">Last updated 3 mins ago</p>
+            </div>
+            <a href="javascript:void()" class="btn btn-primary">Go somewhere</a>
+          </div>
+      </div>
+    </div>
+    <div class="col-xl-4 col-xxl-6 col-lg-6 col-sm-6 ">
+      <div class="card">
+          <div class="card-header">
+            <h5 class="card-title">Card title</h5>
+          </div>
+          <div class="card-body">
+            <p class="card-text">This is a wider card with supporting text and below as a natural lead-in to the additional content. This content is a little <br> bit longer. Some quick example text to build the bulk</p>
+          </div>
+          <div class="card-footer d-sm-flex justify-content-between">
+            <div class="card-footer-link mb-4 mb-sm-0">
+              <p class="card-text text-dark d-inline">Last updated 3 mins ago</p>
+            </div>
+            <a href="javascript:void()" class="btn btn-primary">Go somewhere</a>
+          </div>
+      </div>
+    </div>
+    <div class="col-xl-4 col-xxl-6 col-lg-6 col-sm-6 ">
+      <div class="card">
+          <div class="card-header">
+            <h5 class="card-title">Card title</h5>
+          </div>
+          <div class="card-body">
+            <p class="card-text">This is a wider card with supporting text and below as a natural lead-in to the additional content. This content is a little <br> bit longer. Some quick example text to build the bulk</p>
+          </div>
+          <div class="card-footer d-sm-flex justify-content-between">
+            <div class="card-footer-link mb-4 mb-sm-0">
+              <p class="card-text text-dark d-inline">Last updated 3 mins ago</p>
+            </div>
+            <a href="javascript:void()" class="btn btn-primary">Go somewhere</a>
+          </div>
+      </div>
+    </div>
+    <div class="col-xl-4 col-xxl-6 col-lg-6 col-sm-6 ">
+      <div class="card">
+          <div class="card-header">
+            <h5 class="card-title">Card title</h5>
+          </div>
+          <div class="card-body">
+            <p class="card-text">This is a wider card with supporting text and below as a natural lead-in to the additional content. This content is a little <br> bit longer. Some quick example text to build the bulk</p>
+          </div>
+          <div class="card-footer d-sm-flex justify-content-between">
+            <div class="card-footer-link mb-4 mb-sm-0">
+              <p class="card-text text-dark d-inline">Last updated 3 mins ago</p>
+            </div>
+            <a href="javascript:void()" class="btn btn-primary">Go somewhere</a>
+          </div>
+      </div>
+    </div>
+    <div class="col-xl-4 col-xxl-6 col-lg-6 col-sm-6 ">
+      <div class="card">
+          <div class="card-header">
+            <h5 class="card-title">Card title</h5>
+          </div>
+          <div class="card-body">
+            <p class="card-text">This is a wider card with supporting text and below as a natural lead-in to the additional content. This content is a little <br> bit longer. Some quick example text to build the bulk</p>
+          </div>
+          <div class="card-footer d-sm-flex justify-content-between">
+            <div class="card-footer-link mb-4 mb-sm-0">
+              <p class="card-text text-dark d-inline">Last updated 3 mins ago</p>
+            </div>
+            <a href="javascript:void()" class="btn btn-primary">Go somewhere</a>
+          </div>
+      </div>
+    </div>
+    <div class="col-xl-4 col-xxl-6 col-lg-6 col-sm-6 ">
+      <div class="card">
+          <div class="card-header">
+            <h5 class="card-title">Card title</h5>
+          </div>
+          <div class="card-body">
+            <p class="card-text">This is a wider card with supporting text and below as a natural lead-in to the additional content. This content is a little <br> bit longer. Some quick example text to build the bulk</p>
+          </div>
+          <div class="card-footer d-sm-flex justify-content-between">
+            <div class="card-footer-link mb-4 mb-sm-0">
+              <p class="card-text text-dark d-inline">Last updated 3 mins ago</p>
+            </div>
+            <a href="javascript:void()" class="btn btn-primary">Go somewhere</a>
+          </div>
+      </div>
+    </div>
+  </div>
+ </div>
 <?php
 }
 }
